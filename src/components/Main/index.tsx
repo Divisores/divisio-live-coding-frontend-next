4a960963068a652ff4c8a9afd18523beb69f413a
Rodrigo Wippel <rodrigow1992@gmail.com>
2021-03-21 18:31:46 -0300

    add button

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,19 +1,26 @@
-import * as S from './styles'
-
 import content from './content'
 
-const Main = () => (
-  <S.Container>
-    <S.Title>Pokedex</S.Title>
-    <S.Listing>
-      {content.map((item) => (
-        <S.ListingItem key={item.id}>
-          <S.ListingItemText>{item.name}</S.ListingItemText>
-          <S.ListingItemText>{item.level}</S.ListingItemText>
-        </S.ListingItem>
-      ))}
-    </S.Listing>
-  </S.Container>
-)
+import * as S from './styles'
+
+const Main = () => {
+  const handleAdd = () => {
+    console.log('handleAdd')
+  }
+
+  return (
+    <S.Container>
+      <S.Title>Pokedex</S.Title>
+      <S.Listing>
+        {content.map((item) => (
+          <S.ListingItem key={item.id}>
+            <S.ListingItemText>{item.name}</S.ListingItemText>
+            <S.ListingItemText>{item.level}</S.ListingItemText>
+          </S.ListingItem>
+        ))}
+      </S.Listing>
+      <S.AddButton onClick={handleAdd}>Adicionar pokemon</S.AddButton>
+    </S.Container>
+  )
+}
 
 export default Main
